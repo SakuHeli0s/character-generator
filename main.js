8aae1ef5e2e7f9957e401ebfa6215f60ddd25b17
SakuHeli0s <mariabayanescobar@gmail.com>
2026-03-10 20:10:19 +0100

    Javascript iniciado

---
+++ main.js
@@ -0,0 +1,20 @@
+//CARGAR ELEMENTOS HTML
+
+const input = document.getElementById("input-name")
+const charaOption = document.getElementById("chara-option")
+const btnChara = document.getElementById("btn-chara")
+
+const charaContainer = document.getElementById("chara-container")
+const charaName = document.getElementById("chara-name")
+const statClass = document.getElementById("stat-class")
+const statHp = document.getElementById("stat-hp")
+const statMp = document.getElementById("stat-mp")
+const statStr = document.getElementById("stat-str")
+const statDef = document.getElementById("stat-def")
+
+
+//CODIGO FORMULARIO
+
+btnChara.addEventListener("click", () => {
+    
+})
