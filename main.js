1a347c116ac60205dc1a51d69a7b5bafc30ff376
SakuHeli0s <mariabayanescobar@gmail.com>
2026-03-11 12:35:56 +0100

    Proyecto terminado. Falta retocar css

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const charaOption = document.getElementById("chara-option")
 const btnChara = document.getElementById("btn-chara")
 
 const charaContainer = document.getElementById("chara-container")
+const charaImg = document.getElementById("chara-img")
 const charaName = document.getElementById("chara-name")
 const statClass = document.getElementById("stat-class")
 const statHp = document.getElementById("stat-hp")
@@ -12,9 +13,35 @@ const statMp = document.getElementById("stat-mp")
 const statStr = document.getElementById("stat-str")
 const statDef = document.getElementById("stat-def")
 
+const imagenes = {
+    bruja: "./img/bruja.jpg",
+    arquero: "./img/arquero.jpg",
+    maga: "./img/maga.jpeg",
+    caballero: "./img/caballero.jpeg",
+}
+
 
 //CODIGO FORMULARIO
 
 btnChara.addEventListener("click", () => {
-    
+    let name = input.value 
+    let clase = charaOption.value
+    let vida = Math.floor(Math.random() * 90) + 10
+    let magia = Math.floor(Math.random() * 90) + 10
+    let fuerza = Math.floor(Math.random() * 90) + 10
+    let defensa = Math.floor(Math.random() * 90) + 10
+
+    charaName.textContent = `Nombre: ${name}`
+    statClass.textContent = `Clase: ${clase}`
+    charaImg.src = imagenes[clase]
+    console.log(imagenes[clase])
+    console.log(charaImg)
+    statHp.textContent = `Vida: ${vida}`
+    statMp.textContent = `Magia: ${magia}`
+    statStr.textContent = `Fuerza: ${fuerza}`
+    statDef.textContent = `Defensa: ${defensa}`
+
+    charaContainer.style.display = "block"
+
+    console.log(clase)
 })
